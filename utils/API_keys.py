9b391abc427871c067d9473c424c45b3371b27c1
MatthewDaffern <38901216+MatthewDaffern@users.noreply.github.com>
2018-08-02 21:35:35 -0700

    added basic API key loading framework

--- utils/API_keys.py
+++ utils/API_keys.py
@@ -1,8 +1,20 @@
 def Biblia():
 	#parse the key from a text file that's in the .gitignore
-	key=#store the key here
+	BibliaKeyFile=open('API_Keys','r+')
+	key=str()
+	for i in BibliaKeyFile:
+		if  'Biblia' in str(i):
+			KeyTextLine=str(i)
+			KeyTextLine=KeyTextLine.split(",")
+			key=KeyTextLine[1]
 	return key
 def BibleAPI():
 	#parse the key from a text file that's in the .gitignore
-	key=#store the key here
+    BibleAPIKeyFile=open('API_Keys','r+')
+	key=str()
+	for i in BibleAPIFile:
+		if  'Biblia' in str(i):
+			KeyTextLine=str(i)
+			KeyTextLine=KeyTextLine.split(",")
+			key=KeyTextLine[1]
 	return key
