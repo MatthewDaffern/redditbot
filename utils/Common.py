4ff9aba3ac446caac99889b302cabe90c44d88c6
MatthewDaffern <38901216+MatthewDaffern@users.noreply.github.com>
2018-09-04 16:31:52 -0700

    added some stuff for ESV

--- utils/Common.py
+++ utils/Common.py
@@ -5,12 +5,10 @@ import API_Keys
 #Separate flow control from the lower order functions.
 #Use higher order functions for the flow control decision
 #So this means the following:
-#   Remove the !!!! flow control symbol I'm using
 #   Abstract away each API service functions into a single function for each API.
 #   This will make it easier to perform my unit testing
-#   Create a flow control function that parses the comment to decide what API service should be used
 #   Eventually operate it off a single function.
-#   Remove all global variables
+
 
 '''API Key Storage Functions'''
 def Biblia_API_Key_Storage():
@@ -36,30 +34,45 @@ def comment_parser(input_string):
     query[1]=Verse_Chapter
     #Should now be ['John','3.16',"KJV"]
     return query
-'''Biblia Functions'''
-def Biblia_Text_Creator(Response_Builder):
+'''Bible Functions'''
+def ESV_Response_Builder(query,APIkey):
+    mode='text'
+    url='https://api.esv.org/v3/passage/'+mode+'/?q'+query[0]+'+'+query[1]
+    headers={'authentication':str(' Token '+' {{ '+APIkey+' }} ')}
+    esv_api_call=requests.get(url, headers=headers)
+    return esv_api_call
+
+def Text_Creator(Response_Builder):
     return Response_Builder.text
 def Biblia_Response_Builder(query,APIkey):
     #example API call
     #https://api.biblia.com/v1/bible/content/{bible}.{outputFormat}?passage={bibleReference}&key={API key}
     #expects the query to be in the following format KJV,John,3:16
-        api_call='https://api.biblia.com/v1/bible/content/'+str(query[0])+'.js'+'?passage='+query[1]+query[2]+'&key='+APIkey
-        api_return=requests.get(api_call)
-        return api_return
+        esv_api_call='https://api.biblia.com/v1/bible/content/'+str(query[0])+'.js'+'?passage='+query[1]+query[2]+'&key='+APIkey
+        biblia_api_return=requests.get(api_call)
+        return biblia_api_return
 def Final_Biblia_Response(input):
     Biblia_Response=input
     Biblia_Response_Stripped_Front=Biblia_Response.strip('"myCallbackFunction({"text":"')
     Biblia_Response_Stripped_Back=Biblia_Response_Stripped_Front.strip('"});')
     Biblia_Response_Final=Biblia_Response_Stripped_Back
-    Biblia_Response_Body=Biblia_Response_Final+"!!!!Biblia"
+    Biblia_Response_Body=Biblia_Response_Final+
     return Biblia_Response_Body
 def Biblia_Footer():
     Couple_of_Spaces="\n \n"
-    TOS_footer="^(This) ^(Bot) ^(uses) ^(the) [^(Biblia)](https://biblia.com/) ^(web) ^(services) ^(from) [^(Logos Bible Software)](https://www.logos.com/))"
+    TOS_footer="^(This) ^(Bot) ^(uses) ^(the) [^(Biblia)](https://biblia.com/) ^(web) ^(services) ^(from) [^(ESV)](https://www.logos.com/))"
     Github_Footer="^(|) [^(Source Code)](https://github.com/MatthewDaffern/redditbot])"
     Message_The_Developers_Footer+'^(|) [^(Message the developers)](https://www.reddit.com/message/compose?to=/r/scripturebot)'
     footer=Couple_of_Spaces+TOS_footer+Github_Footer+Message_The_Developers_Footer
-    return footer     
+    return footer 
+def ESV_footer():
+    ESV="*ESV*"
+    Couple_of_Spaces="\n \n"
+    TOS_footer="^(This) ^(Bot) ^(uses) ^(the) [^(ESV)](https://api.esv.org/docs/) ^(web) ^(services) ^(from) [^(Logos Bible Software)](https://www.logos.com/))"
+    Github_Footer="^(|) [^(Source Code)](https://github.com/MatthewDaffern/redditbot])"
+    Message_The_Developers_Footer+'^(|) [^(Message the developers)](https://www.reddit.com/message/compose?to=/r/scripturebot)'
+    footer=ESV+Couple_of_Spaces+TOS_footer+Github_Footer+Message_The_Developers_Footer
+    return footer 
 '''Error Handling Functions'''
 def API_Error_Handler(Response_Builder):
     #expects a requests object as input
@@ -78,32 +91,26 @@ def length_checker(final_comment):
         return "Error:your request could not be fulfilled to to the size constraint on reddit comments. It might be best to paste from your favorite website."
     else:
         return("Success")
-def Format_enforcer:
-    #To do. This will all depend on how the BIBLE API operates.
 '''Comment Building Functions '''
-def full_comment_string(input_string, Response_Body):
+def biblia_full_comment_string(input_string, Response_Body):
     #input string is the string initially used to make the API call.
     bot_username="/u/scripturebot!"
     converted_query=input_string.strip(bot_username)
-    Response_Body=Response_Body.split("!!!!")
     header=str("*"reconverted_query+"*"+"\n \n")
-    if Response_Body[1]=="Biblia":
-        footer=Biblia_Footer()
-    if Response_Body[1]=="BibleAPI":
-        footer=BibleAPI_Footer()
+    footer=Biblia_Footer()
     final_comment=header+Response_Body_Final+footer
     return final_comment
 
 '''Higher order Functions '''
-def query_wrapper(ESV_API,Biblia_API,ESV_Function,Biblia_Function,ESV_query_flag,input_string):
-    if "ESV" in ESV_query_flag:
-        final_query=ESV_Function(input_string)
-    if "ESV" not in ESV_query_flag
-        final_query=Biblia_Function(input_string)
-    return final_query
 def Biblia(API_Key,input_string):
     #full body of code abstracted
     return response
 def ESV(API_Key, input_string):
     #full body of code abstracted
     return response
+def query_wrapper(ESV_API,Biblia_API,ESV_Function,Biblia_Function,ESV_query_flag,input_string):
+    if "ESV" in ESV_query_flag:
+        final_query=ESV(input_string)
+    if "ESV" not in ESV_query_flag
+        final_query=Biblia(input_string)
+    return final_query
