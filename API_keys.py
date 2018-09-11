083295f19045676552bc7fe1bdb32a0b3d49b44d
MatthewDaffern <38901216+MatthewDaffern@users.noreply.github.com>
2018-09-11 16:02:10 -0700

    fixed the API key imports

--- API_keys.py
+++ API_keys.py
@@ -1,22 +1,24 @@
+
+
 def biblia():
-#parse the key from a text file that's in the .gitignore
-        bibliakeyfile=open('api_keys','r+')
-        key=str()
-        bibliakey=list(bibliakeyfile.readlines())
-        for i in bibliakey:
-                if  'biblia' in str(i):
-                        keytextline=str(i)
-                        keytextline=keytextline.split(",")
-                        key=keytextline[1]
-        return key
+    bibliakeyfile = open('config_file', 'r+')
+    key = str()
+    bibliakey = list(bibliakeyfile.readlines())
+    for i in bibliakey:
+        if 'biblia' in str(i):
+            keytextline = str(i)
+            keytextline = keytextline.split(": ")
+            key = keytextline[1]
+    return key
+
+
 def esv():
-#parse the key from a text file that's in the .gitignore
-        esvkeyfile=open('api_keys','r+')
-        esvkey=list(esvkeyfile.readlines())
-        key=str()
-        for i in esvkey:
-                if  'esv' in str(i):
-                        keytextline=str(i)
-                        keytextline=keytextline.split(",")
-                        key=keytextline[1]
-        return key
+    esvkeyfile = open('config_file', 'r+')
+    esvkey = list(esvkeyfile.readlines())
+    key = str()
+    for i in esvkey:
+        if 'esv' in str(i):
+            keytextline = str(i)
+            keytextline = keytextline.split(": ")
+            key = keytextline[1]
+    return key
